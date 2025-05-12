21297bf3acd1b5d157be60aa12af3be2e172178b
PrKen <duflaut@et.esiea.fr>
2025-05-12 20:15:58 +0200

    Patched file (Profile)

--- src/app/(admin)/(others-pages)/profile/page.tsx
+++ src/app/(admin)/(others-pages)/profile/page.tsx
@@ -3,7 +3,6 @@ import UserInfoCard from "@/components/user-profile/UserInfoCard";
 import UserMetaCard from "@/components/user-profile/UserMetaCard";
 import { Metadata } from "next";
 import React from "react";
-import SaasMetrics from "@/components/challenges/SaasMetrics";
 
 export const metadata: Metadata = {
   title: "Next.js Profile | TailAdmin - Next.js Dashboard Template",
@@ -14,9 +13,6 @@ export const metadata: Metadata = {
 export default function Profile() {
   return (
     <div>
-      <div className="space-y-6">
-          <SaasMetrics />
-      </div>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
         <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
           Profile
