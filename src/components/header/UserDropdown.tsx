4190ce8c11d77f1b9bb85e1f7fc335d6a0787e53
GOMES <mgomes@et.esiea.fr>
2025-05-19 03:06:39 +0200

    dropdown en VF

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -171,10 +171,10 @@ export default function UserDropdown() {
             <DropdownItem
               onItemClick={closeDropdown}
               tag="a"
-              href="/profile"
+              href="/user_profile"
               className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
             >
-              Account settings
+              Profile Utilisateur
             </DropdownItem>
           </li>
 
@@ -192,7 +192,7 @@ export default function UserDropdown() {
           href="/signin"
           className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
         >
-          Sign out
+          Déconnection
         </Link>
         {isAdmin && (
           <DropdownItem
